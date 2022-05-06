77b6ae475cf4690e1254471838d7256ae3036636
jeong9204 <99022588+jeong9204@users.noreply.github.com>
2022-05-06 23:02:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	$('.main_mov').delay(2000).hide(10, '', setTimeout(function(){$('body').removeClass('in')}, 2000));
+	
 	$('#fullpage').fullpage({
 		anchors: ['home', 'skill'],
 		autoScrolling:true,
