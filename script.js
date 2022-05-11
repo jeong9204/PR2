8d94c0b1fd6451eed9084086d1147c4b00e64c89
jeong9204 <99022588+jeong9204@users.noreply.github.com>
2022-05-11 19:50:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ $(document).ready(function() {
 		autoScrolling:true,
 		scrollHorizontally: true
 	});
+
+	$('.skill-box .list li div').hover(function(){
+		//마우스를 올렸을때
+		t = $(this).parent('li');
+		if (t.hasClass('active')) {
+			t.removeClass('active');
+		}else {
+			$(".skill-box .list li").removeClass('active');
+			t.addClass('active');
+			t.find('span.counter').counterUp({
+				delay: 10,
+				time: 1000
+			});	
+		}
+	}, function(){
+		$(".skill-box .list li").removeClass('active');
+	});
 });
